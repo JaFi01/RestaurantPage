a0192ebec7943a765e791610bc98be9703f1343b
JaFi01 <filipek.jaroslaw1@gmail.com>
2024-12-29 10:01:59 +0100

    basic views logic

--- page/views.py
+++ page/views.py
@@ -1,3 +1,27 @@
+from django.views.generic import TemplateView
+from .models import Menu, Kategoria, Alergeny, Promocje
+
 from django.shortcuts import render
 
 # Create your views here.
+
+class MainPageView(TemplateView):
+    template_name = "page/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu_items'] = Menu.objects.all()
+        return context
+
+class MenuPageView(TemplateView):
+    template_name = "page/menu.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu_items'] = Menu.objects.all()
+        context['kategorie'] = Kategoria.objects.all()
+        context['alergeny'] = Alergeny.objects.all()
+        context['promocje'] = Promocje.objects.all()
+        return context
+
+
